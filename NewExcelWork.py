0f84ccf57aba4e0185e0b7616785962580398ec0
Pythoncraft <73964453+Pythoncraft@users.noreply.github.com>
2020-11-12 09:46:44 +0200

    Final commit working

--- NewExcelWork.py
+++ NewExcelWork.py
@@ -1,4 +1,6 @@
 import openpyxl
+
+
 excel_root_files = ['‪C:\\Users\\igori\\Desktop\\Projects\\Offers.xlsx']
 s = '‪C:\\Users\\igori\\Desktop\\Projects\\Offers.xlsx'
 s = s.lstrip('\u202a')
@@ -8,7 +10,7 @@ values = []
 for file in excel_root_files:
 	workbook = openpyxl.load_workbook(s, file)
 	worksheet = workbook['ANDREA PAPADOURI']
-	cell_value = worksheet['E6']
+	cell_value = worksheet['E6'].value
 	values.append(cell_value)
 
 	print(cell_value)
